d0a7964d51ba5dffc182bbf7deab2b949981f029
ryuga0719 <ryuga.private@gmail.com>
2023-11-26 15:35:14 +0900

    replace paths

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from './ui/fonts';
 
 export default function Page() {
   return (
